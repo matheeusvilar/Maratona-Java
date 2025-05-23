5705dacae0103e2652eb4d77a925b4cb1e3e641c
matheeusvilar <matheus.vilar@gmail.com>
2025-05-23 00:04:07 -0300

    Primeiro commit curso JAVA - Subindo curso JAVA

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main (String args[]){
+    //int, double, float, chat, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble  = 2000;
+        double salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        System.out.println("A idade e " + idade+ "10 anos");
+        System.out.println(false);
+    }
+}
