d87d28c522aff6dcb5511a913a5d9709c9d523c0
matheeusvilar <morizono597@gmail.com>
2025-05-28 17:28:39 -0300

    Resolve conflito na aula de estruturas condicionais.
    src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
    Resolve conflito na aula de estruturas condicionais.
    
    src/academy/devdojo/maratonajava/intro:ucao/Aula05EstrutursCondicio

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -4,27 +4,6 @@ import java.sql.SQLOutput;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        //OPERADORES LOGICOS
-        // && (AND) || (OU) (OR) !
-
-        int idade = 24;
-        float salario = 3500F;
-        boolean isDentrodaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
-        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
-
-        System.out.println("isDentrodaLeiMaiorQueTrinta " + isDentrodaLeiMaiorQueTrinta);
-        System.out.println("isDentrodaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
-
-        double valorTotalContaCorrente = 200;
-        double valorTotalContaPoupanca = 10000;
-        float valorPlaystation = 5000;
-
-        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
-        System.out.println("isPlaystationCincoCompravel" + isPlaystationCincoCompravel);
-    }
-}
-=======
 
 
     //+ - / *
@@ -62,4 +41,3 @@ public class Aula04Operadores {
         System.out.println(isDentroDaLeiMenorQueTrinta);
     }
 }
->>>>>>> dba1735 (continued aula Estruturas condicionais)
--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -2,7 +2,6 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
-<<<<<<< HEAD
         //THE FAMOUS SEE (IF)
         int idade = 15;
         boolean isAutorizadoComprarBebida = idade >=18;
@@ -11,12 +10,6 @@ public class Aula05EstruturasCondicionais01 {
             System.out.println("Autorizado para comprar bebida alcoolica.");
         }else{
             System.out.println("Nao autorizado a comprar bebida alcolica");
-=======
-        int idade = 15;
-        if (idade  <= 20){
-            System.out.println("Você é menor de idade");
-
->>>>>>> dba1735 (continued aula Estruturas condicionais)
         }
     }
 }
