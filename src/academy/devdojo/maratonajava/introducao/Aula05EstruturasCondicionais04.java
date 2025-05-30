d61b29f521d5001a703e0201f01d1d0f89087e46
matheeusvilar <morizono597@gmail.com>
2025-05-30 16:20:03 -0300

    Aula05EstruturasCondicionais-fazendo exercicio

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais04 {
+    public static void main(String[] args) {
+        double salarioAnual = 7000 ;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 17.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+        double valorImposto = 0;
+
+        if()
+
+    }
+}
