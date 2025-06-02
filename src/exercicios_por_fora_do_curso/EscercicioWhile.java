b735d844290d98930afab21d10c2b717cffe29cd
matheeusvilar <morizono597@gmail.com>
2025-06-02 17:22:39 -0300

    Começando os Estudos em Estruturas de Repetição

---
+++ src/exercicios_por_fora_do_curso/EscercicioWhile.java
@@ -0,0 +1,17 @@
+package exercicios_por_fora_do_curso;
+
+public class EscercicioWhile {
+    public static void main(String[] args) {
+//        int number = 1;
+//        while(number <= 100){
+//            if (number % 2 != 0){
+//                System.out.println(number);
+//            }
+//            number++; //soma +1 pra avançar
+//        }
+
+
+
+
+    }
+}
