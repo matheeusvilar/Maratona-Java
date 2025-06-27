dcdb343844748da6cb1a266ff383cc2cef018c3a
matheeusvilar <morizono597@gmail.com>
2025-06-27 17:35:07 -0300

    att

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -2,6 +2,11 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Calculadora {
     public void somaDoisNumero(){
+
         System.out.println(10+10);
+    }
+
+    public void subtracaoMenosNumero(){
+        System.out.println(21-12);
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -4,6 +4,16 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculad
 
 public class CalculadoraTest01 {
     public static void main(String[] args) {
+       //Calculadora = IMPORTANDO A CLASSE
+        //calculadora = CRIANDO VARIAVEL DE REFERENCIA
+        // new Calculadora (); = CRIANDO O OBJETO
+        Calculadora calculadora = new Calculadora();
+        //EXECUTANDO METODO
+        calculadora.somaDoisNumero();
+        System.out.println("Finalizando calculadora teste");
+
+        calculadora.subtracaoMenosNumero();
+
 
     }
 }
