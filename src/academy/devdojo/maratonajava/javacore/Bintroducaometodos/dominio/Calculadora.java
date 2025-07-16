fc7d731a114161de906e4891a2aacd5f4fc12f1b
matheeusvilar <morizono597@gmail.com>
2025-07-16 12:58:47 -0300

    Objetos - Métodos pt 04 - Retorno pt 02

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -23,5 +23,11 @@ public class Calculadora {
         return num1/num2;
     }
 
+    public void imprimeDvisaoDeDoisNumeros(double num1, double num2) {
+        if(num2 == 0){
+            System.out.println("Não existe divisão por zero");
+        }
+        System.out.println(num1/num2);
+    }
 
 }
