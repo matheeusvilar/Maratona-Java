637a6bdf311617db3875014cd067f29e1a745900
matheeusvilar <morizono597@gmail.com>
2025-07-17 16:28:37 -0300

    Aula 48 metodos pt05 - parametros tipo primitivo

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -24,10 +24,17 @@ public class Calculadora {
     }
 
     public void imprimeDvisaoDeDoisNumeros(double num1, double num2) {
-        if(num2 == 0){
+        if (num2 == 0) {
             System.out.println("Não existe divisão por zero");
+            return;
+
         }
-        System.out.println(num1/num2);
+        System.out.println(num1 / num2);
     }
 
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+
+    }
 }
