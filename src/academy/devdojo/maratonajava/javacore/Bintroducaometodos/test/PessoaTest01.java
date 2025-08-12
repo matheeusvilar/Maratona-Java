8d1ffd709ff823cb705d30b1cbf40ce0af498328
matheeusvilar <matheus.vilar@gmail.com>
2025-08-12 01:13:05 -0300

    Modificadores de acesso

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+//        pessoa.nome = "Matheus";
+//        pessoa.idade = 24;
+        pessoa.setNome("Matheus");
+        pessoa.setIdade(24);
+//        pessoa.imprime();
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+
+    }
+}
