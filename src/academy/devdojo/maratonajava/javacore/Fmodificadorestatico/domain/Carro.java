83cb726957014594b215841b9c706b9f1d543849
matheeusvilar <morizono597@gmail.com>
2025-08-20 13:00:20 -0300

    Modificador static - aula 61 - parte do diagrama

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
@@ -0,0 +1,43 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    private double velocidadeLimite = 250;
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+
+    }
+
+    public void imprime(){
+        System.out.println("--------------");
+        System.out.println("Nome "+this.nome);
+        System.out.println("Velocidade Maxima "+this.velocidadeMaxima);
+        System.out.println("Velocidade Limite "+this.velocidadeLimite);
+    }
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
