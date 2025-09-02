5e0956e83e4e7219ee283d937268453b3f96c007
matheeusvilar <morizono597@gmail.com>
2025-09-02 14:11:19 -0300

    Exercicio

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/Test/LeituraDoTecladoTest02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.Test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("O grande software de previsão do futuro");
+        System.out.println("Digite sua pergunta e eu responderei sim ou não");
+        String pergunta = input.nextLine();
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Seminario/Dominio/Seminario.java
@@ -0,0 +1,6 @@
+package academy.devdojo.maratonajava.javacore.Seminario.Dominio;
+
+public class Seminario {
+    private String aluno;
+
+}
