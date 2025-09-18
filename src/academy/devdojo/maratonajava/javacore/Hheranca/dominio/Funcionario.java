5397c1dcd75ceafb09f5ebe450837e8f5fe9ae2b
Matheus Vilar <matheus.vilar1006@gmail.com>
2025-09-18 17:09:21 -0300

    Primeiro commit no Mac

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -2,13 +2,17 @@ package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
 // (EXTENDS) estou dizendo que o funcionario vai ter todos os atributos e metodos que uma pessoa tem ate pq um funcionario e uma pessoa
 public class Funcionario extends Pessoa {
 
-    private double Salario;
+    private double salario;
+
+    public void imprime() {
+        System.out.println(this.salario );
+    }
 
     public double getSalario() {
-        return Salario;
+        return salario;
     }
 
     public void setSalario(double salario) {
-        Salario = salario;
+        salario = salario;
     }
 }
