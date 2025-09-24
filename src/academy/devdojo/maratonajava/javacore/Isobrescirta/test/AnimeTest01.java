3b102424e7379dd8ad0adebed27b15554e393d12
Matheus Vilar <matheus.vilar1006@gmail.com>
2025-09-24 12:58:21 -0300

    Help me

---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescirta/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Isobrescirta.test;
+
+import academy.devdojo.maratonajava.javacore.Isobrescirta.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Nanami");
+        System.out.println(anime);
+    }
+}
