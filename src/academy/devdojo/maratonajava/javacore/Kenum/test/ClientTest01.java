3f289db86109f7ed5c9d8d66c136f307087097e1
matheeusvilar <morizono597@gmail.com>
2025-10-07 12:55:28 -0300

    Continued in aula 81

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
@@ -1,12 +1,14 @@
 package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClientTest01 {
     public static void main(String[] args) {
-        Cliente cliente1  = new Cliente("Nanami", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2  = new Cliente("Jogo", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1  = new Cliente("Nanami", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2  = new Cliente("Jogo", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
         System.out.println(cliente1);
         System.out.println(cliente2);
     }
