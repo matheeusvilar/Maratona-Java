7ddeb31eac629ff8d285c510edc16b17f3f57c68
matheeusvilar <morizono597@gmail.com>
2025-10-15 12:55:53 -0300

    Continued aula 83

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
@@ -1,8 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
-import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClientTest01 {
     public static void main(String[] args) {
@@ -11,5 +11,6 @@ public class ClientTest01 {
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
     }
 }
